d89188713d952989327c2da2e68fc56b0bfbeb4b
unknown <mohd.miraj@digimantra.com>
2019-11-19 22:30:29 +0530

    first

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
+        </div>
+    )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
+        </div>
+    )
+}
+
+export default Contact;
